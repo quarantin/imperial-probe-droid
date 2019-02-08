82dfca75830ee9bdac998fcc26c958d2bb46a5aa
Corentin Delorme <codelorme@gmail.com>
2019-02-08 15:20:18 +0100

    Update config.py: Change delimiter length from 24 to 27

--- config.py
+++ config.py
@@ -207,6 +207,6 @@ def load_config(bot=None, config_file='config.json'):
 		config['help'] = load_help()
 		config['recos'] = parse_recommendations()
 		config['save'] = save_config
-		config['separator'] = '`%s`' % ('-' * 24)
+		config['separator'] = '`%s`' % ('-' * 27)
 
 	return config
