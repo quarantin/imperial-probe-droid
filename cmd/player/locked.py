4138cd878746a3863ec112eb32fbb31045e59782
Corentin Delorme <codelorme@gmail.com>
2019-02-13 14:03:39 +0100

    Update cmd/player/locked.py: Switch to new API from swgoh.help

--- cmd/player/locked.py
+++ cmd/player/locked.py
@@ -1,62 +1,73 @@
 #!/usr/bin/python3
 
 from opts import *
-from swgoh import *
+from swgohgg import get_char_list, get_ship_list
+from swgohhelp import fetch_players, fetch_units, get_arena_rank, get_player_name
 
 help_locked = {
-	'title': 'Player info',
-	'description': """Shows units and ships that are still locked for the supplied ally codes.
+	'title': 'Player Command',
+	'description': """Shows characters and ships that are still locked for the supplied players.
 
 **Syntax**
 ```
-%prefixlocked [ally codes or mentions]
-%prefixlocked [ally codes or mentions] [options]```
+%prefixlocked [players] [options]```
 **Aliases**
 ```
 %prefixl```
 
 **Options**
-```
-units (or u)
-ships (or s)```
+The following options are available to filter output:
+**`chars`** (or **`c`**): To show locked characters for the specified players.
+**`ships`** (or **`s`**): To show locked ships for the specificed players.
+If no options are specified, all locked characters and ships are displayed.
 **Examples**
-Show your locked units and ships:
+Show your locked characters and ships:
 ```
 %prefixl```
-Show only your locked units:
+Show only your locked characters:
 ```
-%prefixl u```
+%prefixl c```
 Show only your locked ships:
 ```
 %prefixl ships```
-Show locked units and ships for someone by mention:
+Show locked characters and ships for someone by mention:
 ```
 %prefixl @Someone```
-Show locked units and ships for someone by discord-nick (no hilight):
+Show locked characters and ships for someone by discord-nick (no hilight):
 ```
 %prefixl "Someone"```
-Show locked units for someone by ally code:
+Show locked characters for someone by ally code:
 ```
-%prefixl 123456789 units```
-Show locked units and ships for two different ally codes:
+%prefixl 123456789 chars```
+Show locked characters and ships for two different ally codes:
 ```
 %prefixl 123456789 234567891```"""
 }
 
-def parse_opts_units_or_ships(args):
+opts_locked = {
+	'c':     'chars',
+	'chars': 'chars',
+	's':     'ships',
+	'ships': 'ships',
+}
 
-	opts = 'all'
+def parse_opts_locked(args):
+
+	opts = []
 	args_cpy = list(args)
 
 	for arg in args_cpy:
 
-		if arg in [ 'u', 'units' ]:
-			opts = 'units'
+		if arg in opts_locked:
 			args.remove(arg)
+			opt = opts_locked[arg]
+			if opt not in opts:
+				opts.append(opt)
 
-		elif arg in [ 's', 'ships' ]:
-			opts = 'ships'
-			args.remove(arg)
+	if not opts or len(opts) >= 2:
+		opts = 'all'
+	else:
+		opts = opts.pop(0)
 
 	return args, opts
 	
@@ -67,10 +78,10 @@ def cmd_locked(config, author, channel, args):
 		return [{
 			'title': 'Not Found',
 			'color': 'red',
-			'description': 'No ally code specified, or found registered to <%s>' % author,
+			'description': 'No ally code specified or found registered to <%s>' % author,
 		}]
 
-	args, opts = parse_opts_units_or_ships(args)
+	args, opts = parse_opts_locked(args)
 	if args:
 		plural = len(args) > 1 and 's' or ''
 		return [{
@@ -79,51 +90,62 @@ def cmd_locked(config, author, channel, args):
 			'description': 'I don\'t know what to do with the following parameter%s:\n - %s' % (plural, '\n - '.join(args)),
 		}]
 
-	all_units = get_all_units()
-	all_ships = get_all_ships()
+	all_ships = get_ship_list()
+
+	players = fetch_players(config, ally_codes, 'name')
+	units = fetch_units(config, ally_codes)
 
 	msgs = []
 	for ally_code in ally_codes:
 
-		player = get_player_name(ally_code)
+		ally_code = int(ally_code)
+		ally_units = units[ally_code]
+
+		player = get_player_name(config, ally_code)
 		if not player:
 			url = 'https://swgoh.gg/p/%s/' % ally_code
 			msgs.append({
 				'title': 'Not Found',
 				'color': 'red',
-				'description': 'Are you sure `%s` is a valid ally code and the account actually exists on swgoh.gg? Please check this URL to see: %s' % (ally_code, url)
+				'description': 'Are you sure `%s` is a valid ally code and the account actually exists on swgoh.gg? Please visit this URL to check: %s' % (ally_code, url)
 			})
 			continue
 
-		ally_db = get_my_units_and_ships(ally_code)
+		if opts in [ 'chars', 'all' ]:
 
-		if opts in [ 'units', 'all' ]:
+			locked_chars = []
+			for char in get_char_list():
+				base_id = char['base_id']
+				if base_id not in ally_units:
+					locked_chars.append(char['name'])
 
-			locked_units = []
-			for base_id, unit in all_units.items():
-				if base_id not in ally_db['units']:
-					locked_units.append(unit['name'])
-
-			if not locked_units:
-				locked_units.append('All units unlocked!')
+			if not locked_chars:
+				locked_chars.append('All units unlocked!')
 
 			msgs.append({
-				'title': 'Locked units for %s' % player,
-				'description': '%s\n%s' % (config['separator'], '\n'.join(locked_units)),
+				'title': 'Locked Characters',
+				'author': {
+					'name': '%s' % player,
+				},
+				'description': '%s\n%s' % (config['separator'], '\n'.join(locked_chars)),
 				})
 
 		if opts in [ 'ships', 'all' ]:
 
 			locked_ships = []
-			for base_id, ship in all_ships.items():
-				if base_id not in ally_db['ships']:
+			for ship in all_ships:
+				base_id = ship['base_id']
+				if base_id not in ally_units:
 					locked_ships.append(ship['name'])
 
 			if not locked_ships:
 				locked_ships.append('All ships unlocked!')
 
 			msgs.append({
-				'title': 'Locked ships for %s' % player,
+				'title': 'Locked Ships',
+				'author': {
+					'name': '%s' % player,
+				},
 				'description': '%s\n%s' % (config['separator'], '\n'.join(locked_ships)),
 				})
 
