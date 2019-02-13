622fcf22fca993078089e40937c7d19a4b2c54b9
Corentin Delorme <codelorme@gmail.com>
2019-02-13 18:44:26 +0100

    Update swgohhelp.py

--- swgohhelp.py
+++ swgohhelp.py
@@ -1,17 +1,25 @@
 #!/usr/bin/python3
 
+from utils import get_units_dict
+
 import sys
 import requests
 from datetime import datetime, timedelta
 
-db = {}
-
-db['players'] = {}
-db['guilds'] = {}
-db['units'] = {}
-db['roster'] = {}
+db = {
+	'guilds':  {},
+	'players': {},
+	'roster':  {},
+	'units':   {},
+	'events':  {},
+	'zetas':   {},
+	'data':    {},
+}
 
 SWGOH_HELP = 'https://api.swgoh.help'
+
+CRINOLO_PROD_URL = 'https://crinolo-swgoh.glitch.me/statCalc/api/player'
+CRINOLO_TEST_URL = 'https://crinolo-swgoh.glitch.me/testCalc/api/player'
 
 #
 # Internal - Do not call yourself
@@ -21,7 +29,7 @@ def get_access_token(config):
 
 	if 'access_token' in config['swgoh.help']:
 		expire = config['swgoh.help']['access_token_expire']
-		if expire < datetime.now() - timedelta(seconds=60):
+		if expire > datetime.now() + timedelta(seconds=60):
 			return config['swgoh.help']['access_token']
 
 	headers = {
@@ -41,6 +49,9 @@ def get_access_token(config):
 	response = requests.post(auth_url, headers=headers, data=data)
 	data = response.json()
 
+	if 'access_token' not in data:
+		raise Exception('Authentication failed: Server returned `%s`' % data)
+
 	config['swgoh.help']['access_token'] = data['access_token']
 	config['swgoh.help']['access_token_expire'] = datetime.now() + timedelta(seconds=data['expires_in'])
 
@@ -55,6 +66,7 @@ def get_headers(config):
 	}
 
 def call_api(config, project, url):
+	print("CALL API: %s %s" % (url, project))
 	return requests.post(url, headers=get_headers(config), json=project).json()
 
 #
@@ -94,7 +106,7 @@ def fetch_players(config, ally_codes, key='name'):
 	# Remove ally codes for which we already have fetched the data
 	needed = list(ally_codes)
 	for ally_code in ally_codes:
-		if ally_code in db['players'] and key in db['players'][ally_code]:
+		if int(ally_code) in db['players'] and key in db['players'][int(ally_code)]:
 			needed.remove(ally_code)
 
 	if needed:
@@ -106,6 +118,8 @@ def fetch_players(config, ally_codes, key='name'):
 
 		# Store newly needed players in db
 		for player in players:
+			player['roster-by-id'] = get_units_dict(player['roster'], 'defId')
+
 			ally_code = player['allyCode']
 			db['players'][ally_code] = player
 
@@ -195,21 +209,22 @@ def get_player_names(config, ally_codes):
 
 def get_player_name(config, ally_code):
 	data = get_player_names(config, [ ally_code ])
-	return data[ally_code]
+	return data[int(ally_code)]
 
-def get_last_syncs(config, ally_codes):
+def get_last_syncs(config, ally_codes, date_format):
 
 	fetch_players(config, ally_codes, 'updated')
 
 	updated = {}
 	for ally_code in ally_codes:
-		updated[ally_code] = db['players'][ally_code]['updated']
+		ally_code = int(ally_code)
+		updated[ally_code] = datetime.fromtimestamp(int(db['players'][ally_code]['updated']) / 1000).strftime(date_format)
 
 	return updated
 
-def get_last_sync(config, ally_code):
-	data = get_last_syncs(config, [ ally_code ])
-	return data[ally_code]
+def get_last_sync(config, ally_code, date_format):
+	data = get_last_syncs(config, [ ally_code ], date_format)
+	return data[int(ally_code)]
 
 def get_arena_ranks(config, ally_codes, arena_type):
 
@@ -220,10 +235,49 @@ def get_arena_ranks(config, ally_codes, arena_type):
 
 	ranks = {}
 	for ally_code in ally_codes:
+		ally_code = int(ally_code)
 		ranks[ally_code] = db['players'][ally_code]['arena'][arena_type]['rank']
 
 	return ranks
 
 def get_arena_rank(config, ally_code, arena_type):
 	data = get_arena_ranks(config, [ ally_code ], arena_type)
-	return data[ally_code]
+	return data[int(ally_code)]
+
+def get_arena_squads(config, ally_codes, arena_type):
+
+	if arena_type not in [ 'char', 'ship' ]:
+		raise Exception('Invalid arena type: %s. It must be either \'char\' or \'ship\'.' % arena_type)
+
+	fetch_players(config, ally_codes, 'arena')
+
+	squads = {}
+	for ally_code in ally_codes:
+		ally_code = int(ally_code)
+		squads[ally_code] = db['players'][ally_code]['arena'][arena_type]['squad']
+
+	return squads
+
+def get_arena_squad(config, ally_code, arena_type):
+	data = get_arena_squads(config, [ ally_code ], arena_type)
+	print(data[int(ally_code)])
+	return data[int(ally_code)]
+
+def get_stats(config, ally_code):
+
+	stats = {}
+
+	ally_code = int(ally_code)
+	fetch_players(config, [ ally_code ])
+
+	if 'char_stats' in db['players'][ally_code]:
+		return db['players'][ally_code]['char_stats']
+
+	url = '%s/%s/characters?flags=withModCalc' % (CRINOLO_TEST_URL, ally_code)
+	roster = requests.get(url).json()
+	for unit in roster:
+		base_id = unit['defId']
+		stats[base_id] = unit['stats']
+
+	db['players'][ally_code]['char_stats'] = stats
+	return stats
