ca6d31d1940c7f401674a0b6c40727218fa16a18
Corentin Delorme <codelorme@gmail.com>
2019-02-13 19:01:30 +0100

    Update cmd/misc/meta.py: Fix imports from swgohgg

--- cmd/misc/meta.py
+++ cmd/misc/meta.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
 from opts import *
-from swgoh import *
 from utils import lpad
+
+from swgohgg import get_top_rank1_squad_leaders, get_top_rank1_arena_squads, get_top_rank1_fleet_commanders, get_top_rank1_reinforcements, META_UNITS_URL, META_SHIPS_URL
 
 help_meta = {
 	'title': 'Meta Info',
