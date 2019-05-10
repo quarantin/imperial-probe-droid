ec22734b5be54ff4a13a297cd329ab7ab9c186c2
Corentin Delorme <codelorme@gmail.com>
2019-05-10 12:33:37 +0200

    Refactor ipd.py

--- ipd.py
+++ ipd.py
@@ -3,12 +3,14 @@
 import asyncio 
 
 from bot import bot
-from config import config, load_config
+from config import load_config
 
 try:
-	load_config()
+	config = load_config()
 
-	bot.loop.run_until_complete(bot.start(config['token']))
+	bot.run(config['token'])
+
+	print('Bot quitting')
 
 except Exception as err:
 	print(err)
