1d3d065d6821ceb01bba6ebc8f1922dbef09ad35
Corentin Delorme <codelorme@gmail.com>
2019-05-11 15:29:57 +0200

    Update django/scripts/init-database.sh: Dont load init.py by default

--- django/scripts/init-database.sh
+++ django/scripts/init-database.sh
@@ -2,4 +2,4 @@
 
 rm -f db.sqlite3
 rm -rf */migrations/
-python3 manage.py makemigrations && python3 manage.py migrate --run-syncdb && python3 manage.py shell < init.py
+python3 manage.py makemigrations && python3 manage.py migrate --run-syncdb #&& python3 manage.py shell < init.py
