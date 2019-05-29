41328890238a4184d77d560858d33b686e614f16
Corentin Delorme <codelorme@gmail.com>
2019-05-29 11:03:59 +0200

    Update scripts/init-database.sh: Backup old database with timestamp

--- scripts/init-database.sh
+++ scripts/init-database.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-rm -f db.sqlite3
+DATE=$(date '+%Y%m%d-%H%M%S')
+mv db.sqlite3 "${DATE}_db.sqlite3"
 rm -rf */migrations/
 python3 manage.py makemigrations && python3 manage.py migrate --run-syncdb #&& python3 manage.py shell < init.py
