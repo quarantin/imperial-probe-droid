23563292cab94fc87c17537ccbb62e7d268963c0
Corentin Delorme <codelorme@gmail.com>
2019-05-31 18:08:15 +0200

    Update examples/test-guild.py: Stringify ally code

--- examples/test-guild.py
+++ examples/test-guild.py
@@ -5,6 +5,6 @@ from config import load_config
 from swgohhelp import fetch_guilds
 
 config = load_config()
-ally_codes = [ 349423868 ]
+ally_codes = [ '349423868' ]
 result = fetch_guilds(config, ally_codes)
 print(json.dumps(result, indent=4))
