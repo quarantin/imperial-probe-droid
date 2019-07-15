493922b13479e5a6dac7ef1ce29ccf58d01c3eac
Corentin Delorme <codelorme@gmail.com>
2019-07-15 15:21:46 +0200

    Update django settings: Add /media to URL list

--- avatars/urls.py
+++ avatars/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views
 
@@ -23,4 +25,4 @@ urlpatterns = [
 	path('avatar/<str:portrait>', views.avatar),
 	path('stats/<str:portrait>/<str:ally_code>/', views.stats),
 	path('swgoh/', include('swgoh.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
