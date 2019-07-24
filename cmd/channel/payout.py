7214955bcd4936d029a16c16650a487830de0021
Corentin Delorme <codelorme@gmail.com>
2019-07-24 19:02:53 +0200

    Update cmd/channel/payout.py: Add missing players with no payout time set

--- cmd/channel/payout.py
+++ cmd/channel/payout.py
@@ -325,10 +325,9 @@ def handle_payout_stats(config, author, channel, args):
 	po_times = list(players_by_payout)
 	none_entry = False
 	if None in po_times:
-		none_entry = po_times.remove(None)
+		po_times.remove(None)
 	po_times.sort()
-	if none_entry:
-		po_times.append(none_entry)
+	po_times.append(None)
 	for diff_time in po_times:
 		players = players_by_payout[diff_time]
 		for p in players:
