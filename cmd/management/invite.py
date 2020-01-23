f3f85ddfd96ea78b814d617e870194eb2e080b6e
Corentin Delorme <codelorme@gmail.com>
2020-01-23 16:50:27 +0100

    Update cmd/management/invite.py: Generate dynamic invite links with correct permssions.

--- cmd/management/invite.py
+++ cmd/management/invite.py
@@ -1,3 +1,5 @@
+from config import get_perms
+from urllib.parse import urlencode
 
 help_invite = {
 	'title': 'Invite Help',
@@ -12,7 +14,13 @@ def cmd_invite(request):
 
 	config = request.config
 
+	invite_url = 'https://discordapp.com/api/oauth2/authorize?' + urlencode({
+		'client_id': config['bot'].user.id,
+		'perms': get_perms(),
+		'scope': 'bot',
+	})
+
 	return [{
 		'title': 'Invite Imperial Probe Droid',
-		'description': '%s\n[Invite this bot to your server](%s)' % (config['separator'], config['invite']),
+		'description': '%s\n[Invite this bot to your server](%s)' % (config['separator'], invite_url),
 	}]
