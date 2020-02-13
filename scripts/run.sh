45b9f4c5aab4148f5f9d36664d2662706c02dbbd
Corentin Delorme <codelorme@gmail.com>
2020-02-13 17:50:50 +0100

    Update scripts/run.sh: Redirect stdout and stderr to different files.

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 SLEEP=1
+ERROR='error.log'
+OUTPUT='output.log'
 NAME='Imperial Probe Droid'
 IPD='ipd.py'
 
 while true; do
 	echo "INFO: Starting ${NAME}..."
-	python ${IPD}
+	(unbuffer python ${IPD} | tee -a "${OUTPUT}") 3>&1 1>&2 2>&3 | tee -a "${ERROR}"
 	if [ "${?}" -ne "0" ]; then
 		echo "FATAL: ${NAME} just crashed\!"
 	fi
