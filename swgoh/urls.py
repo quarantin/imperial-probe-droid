f8208c5b478e36663896e73a3ba1c25c76bb6595
Corentin Delorme <codelorme@gmail.com>
2020-03-03 11:51:25 +0100

    Update swgoh/urls.py: Add login/success view.

--- swgoh/urls.py
+++ swgoh/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import TemplateView
 
 from . import views
 
 urlpatterns = [
+	path('login', TemplateView.as_view(template_name='login.html')),
+	path('success', TemplateView.as_view(template_name='success.html')),
 ]
