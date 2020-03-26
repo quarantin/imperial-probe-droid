b2431a8b1bb817b1d38def043afa2711e8450094
Corentin Delorme <codelorme@gmail.com>
2020-03-26 14:19:10 +0100

    Update scripts/crawler.sh: Don't log output anymore because we use logger now

--- scripts/crawler.sh
+++ scripts/crawler.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
 SLEEP=1
-ERROR='logs/crawler-error.log'
-OUTPUT='logs/crawler-output.log'
 NAME='IPD Crawler'
 MAIN='crawler/crawler.py'
 
 while true; do
 	echo "INFO: Starting ${NAME}..."
-	(PYTHONPATH=.:$PYTHONPATH unbuffer python ${MAIN} | tee -a "${OUTPUT}") 3>&1 1>&2 2>&3 | tee -a "${ERROR}"
+	PYTHONPATH=.:$PYTHONPATH python ${MAIN}
 	if [ "${?}" -ne "0" ]; then
 		echo "FATAL: ${NAME} just crashed\!"
 	fi
