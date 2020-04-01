adeba39f797d42e94367283e14874963f1f23889
Corentin Delorme <codelorme@gmail.com>
2020-04-01 11:32:13 +0200

    Update cmd/general/help.py: Refactor help sections + add invite link

--- cmd/general/help.py
+++ cmd/general/help.py
@@ -1,4 +1,5 @@
 from errors import *
+from utils import get_invite_link
 
 help_help = {
 	'title': 'Imperial Probe Droid Help - Prefix: %prefix',
@@ -6,33 +7,30 @@ help_help = {
 **Source Code**: %source
 **Need support?**: Join us on [Discord](%discord)!
 **Like this bot?**: Support us on [Patreon](%patreon)!
+**Invite this bot?**: Click %invite!
 %separator
-**Help Commands**
+**General Commands**
 **`help`**: This help menu.
 **`help <command>`**: Get help menu for command.
-%separator
-**Management Commands**
-**`alias`**: Manage command aliases.
-**`invite`**: Invite this bot.
-**`nicks`**: Manage nicknames for units and ships.
-**`register`**: Register your ally code.
-**`gregister`**: Register your guild (**NEW**).
 **`config`**: Configure server settings such as bot prefix (**NEW**).
+**`register`**: Register your ally code.
+**`gregister`**: Register your guild mates (**NEW**).
+**`invite`**: Invite this bot to your server.
+%separator
+**Channel Commands**
+**`news`**: Keep track of official news from the game developers (**NEW**).
+**`payout`**: Keep track of time left to payout for your shard members in arena.
 %separator
 **Guild Commands**
 **`gc`**: Guild comparison by roster.
 **`gs`**: Guild comparison by stats.
 **`glist`**: List guild members by unit GP, gear, level, rarity.
 %separator
-**Channel Commands**
-**`news`**: Keep track of official news from the game developers (**NEW**).
-**`payout`**: Keep track of time left to payout for your shard members in arena.
-%separator
 **Player Commands**
 **`arena`**: Show arena squad details.
 **`gac`**: Compare GAC statistics of different players (**NEW**).
-**`gear`**: List needed pieces of gear for units.
-**`gear13`**: List most popular gear 13 for your characters (**NEW**).
+**`gear`**: List equipment needed for characters.
+**`gear13`**: List most popular gear 13 characters you don't have (**NEW**).
 **`locked`**: Show locked characters or ships.
 **`mc`**: Show characters with missing mods or having weak mods (**NEW**).
 **`meta`**: Show information about best arena and fleet squads.
@@ -42,7 +40,7 @@ help_help = {
 **`recos`**: Show information about recommended mods.
 **`relics`**: List most popular characters with relics (**NEW**).
 **`wntm`**: List characters who needs mods with specific criteria.
-**`zetas`**: List most popular zetas for your characters (**NEW**)."""
+**`zetas`**: List most popular zetas you don't have (**NEW**)."""
 #NOT WORKING **`mods`**: Show information about mods.
 #NOT WORKING **`stats`**: Show statistics about equipped mods."""
 }
@@ -52,6 +50,7 @@ def substitute_tokens(config, text):
 	tokens = [
 		'authors',
 		'discord',
+		'invite',
 		'patreon',
 		'prefix',
 		'separator',
@@ -60,9 +59,12 @@ def substitute_tokens(config, text):
 
 	for token in tokens:
 
-		value = config[token]
+		value = token in config and config[token] or ''
 		if token == 'source':
 			value = '[Github](%s)' % value
+
+		elif token == 'invite':
+			value = get_invite_link(config, invite_msg='HERE')
 
 		if type(value) is list:
 			value = ', '.join(value)
