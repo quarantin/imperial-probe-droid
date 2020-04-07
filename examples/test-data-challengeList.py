c37a1896e42d49faaa1d704396206dcb7f7cf13c
Corentin Delorme <codelorme@gmail.com>
2020-04-07 18:09:04 +0200

    Update examples: Add new examples for the /data endpoint

---
+++ examples/test-data-challengeList.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+
+import json
+from config import load_config
+from swgohhelp import api_swgoh_data
+
+async def __main__():
+
+	config = load_config()
+
+	test = await api_swgoh_data(config, {
+		#'collection': 'battleEnvironmentsList',
+		'collection': 'battleTargetingRuleList',
+		'language': 'eng_us',
+		#'language': 'fre_fr',
+		#'project': {
+		#	'nameKey': 1,
+		#	'iconKey': 1,
+		#},
+	})
+	print(json.dumps(test, indent=4))
+	#for data in test:
+	#	print(data)
+
+if __name__ == '__main__':
+	import asyncio
+	asyncio.get_event_loop().run_until_complete(__main__())
---
+++ examples/test-data-xpTableList.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+
+import json
+from config import load_config
+from swgohhelp import *
+
+collection = 'xpTableList'
+language = 'eng_us'
+
+config = load_config()
+
+test = api_swgoh_data(config, {
+	'collection': collection,
+	'language': 'eng_us',
+})
+
+print(json.dumps(test, indent=4))
