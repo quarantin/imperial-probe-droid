9439e54d0834af1fe1a82d025cbbc0277a8d87be
Corentin Delorme <codelorme@gmail.com>
2020-05-06 00:25:01 +0200

    Update crinolo.py: Refactoring. Merge both function into one and add helper functions

--- crinolo.py
+++ crinolo.py
@@ -9,11 +9,49 @@ CRINOLO_URLS = [
 	#'https://swgoh-stat-calc.glitch.me/api',
 ]
 
+def add_pilots(players, units):
+
+	pilots = []
+
+	if not units:
+		return pilots
+
+	# Add pilots of requested ships if missing
+	crews = get_ships_crew()
+	base_ids = [ unit.base_id for unit in units ]
+	for ship_id in list(base_ids):
+		if ship_id in crews:
+			for pilot in crews[ship_id]:
+				if pilot not in base_ids:
+					base_ids.append(pilot)
+					pilots.append(pilot)
+
+	# Remove units not requested
+	for player in players:
+		new_roster = []
+		for unit in list(player['roster']):
+			if unit['defId'] in base_ids:
+				player['roster'].remove(unit)
+
+	return pilots
+
+def remove_pilots(players, pilots):
+
+	# Remove added pilots
+	for player in players:
+		ally_code = player['allyCode']
+		for unit in list(player['roster']):
+			base_id = unit['defId']
+			if base_id in pilots:
+				player['roster'].remove(unit)
+
 async def api_crinolo(players, units=[]):
+
+	pilots = add_pilots(players, units)
 
 	for crinolo_url in CRINOLO_URLS:
 
-		url = '%s?flags=gameStyle' % crinolo_url
+		url = '%s?flags=gameStyle&calcGP' % crinolo_url
 
 		try:
 			data, error = await http_post(url, json=players)
@@ -30,65 +68,10 @@ async def api_crinolo(players, units=[]):
 		if 'error' in data:
 			error = SwgohHelpException()
 			error.title = 'Error from Crinolo API'
-			error.data = data
+			error.data = data;
 			raise error
 
-		return data
+		remove_pilots(players, pilots)
+		return players
 
 	return None
-
-async def fetch_crinolo_stats(players=[], units=[]):
-
-	import DJANGO
-	from swgoh.models import BaseUnitSkill
-
-	all_zetas = list(BaseUnitSkill.objects.filter(is_zeta=1))
-	db = {}
-	for zeta in all_zetas:
-		db[zeta.skill_id] = True
-
-	# Remove units not requested
-	to_remove = []
-	if units:
-
-		crews = get_ships_crew()
-		base_ids = [ unit.base_id for unit in units ]
-		for ship_id in list(base_ids):
-			if ship_id in crews:
-				for pilot in crews[ship_id]:
-					if pilot not in base_ids:
-						base_ids.append(pilot)
-						to_remove.append(pilot)
-
-		for player in players:
-			new_roster = []
-			for unit in player['roster']:
-				if unit['defId'] in base_ids:
-					new_roster.append(unit)
-			player['roster'] = new_roster
-
-	# Add zeta info to skills
-	for player in players:
-		for unit in player['roster']:
-			for skill in unit['skills']:
-				if skill['id'] in db:
-					skill['isZeta'] = True
-
-	stats = await api_crinolo(players)
-
-	result = {}
-	for player in stats:
-		ally_code = player['allyCode']
-		result[ally_code] = {}
-		for unit in player['roster']:
-
-			base_id = unit['defId']
-			if base_id not in result[ally_code] and base_id not in to_remove:
-				result[ally_code][base_id] = unit
-
-			for skill in unit['skills']:
-				if skill['id'] in db:
-					skill['isZeta'] = True
-
-	return result, players
-
