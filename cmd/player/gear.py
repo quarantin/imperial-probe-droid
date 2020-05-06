5728de13788b654bd2981e4466fe3b5250ccf7e3
Corentin Delorme <codelorme@gmail.com>
2020-05-06 15:00:17 +0200

    Update cmd/player/gear.py: Use new swgoh client + Make it possible to select multiple players

--- cmd/player/gear.py
+++ cmd/player/gear.py
@@ -2,8 +2,8 @@ from opts import *
 from errors import *
 from utils import translate
 
+import json
 from swgohgg import get_full_avatar_url
-from swgohhelp import fetch_players
 
 import DJANGO
 from swgoh.models import BaseUnitGear
@@ -56,10 +56,11 @@ async def cmd_gear(request):
 	args = request.args
 	author = request.author
 	config = request.config
+	bot = request.bot
 
 	language = parse_opts_lang(request)
 
-	selected_players, error = parse_opts_players(request, min_allies=1, max_allies=1)
+	selected_players, error = parse_opts_players(request)
 
 	selected_units = parse_opts_unit_names(request)
 
@@ -75,80 +76,68 @@ async def cmd_gear(request):
 	if args:
 		return error_unknown_parameters(args)
 
-	ally_code = selected_players[0].ally_code
-	players = await fetch_players(config, {
-		'allycodes': [ ally_code ],
-		'project': {
-			'allyCode': 1,
-			'name': 1,
-			'roster': {
-				'defId': 1,
-				'gear': 1,
-				'level': 1,
-				'rarity': 1,
-				'relic': 1,
-				'skills': 1,
-				'equipped': {
-					'slot': 1,
-				},
-			},
-		},
-	})
-
+	ally_codes = [ x.ally_code for x in selected_players ]
+	players = await bot.client.players(ally_codes=ally_codes)
+	players = { x['allyCode']: x for x in players }
 
 	msgs = []
-	player = players[ally_code]
-	for unit in selected_units:
+	for player in selected_players:
 
-		lines = []
-		fields = []
-		player_unit = unit.base_id in player['roster'] and player['roster'][unit.base_id] or {}
-		json = get_gear_levels(unit.base_id)
-		for name, data in json.items():
-			unit_name = translate(unit.base_id, language)
-			lines.append('**[%s](%s)**' % (unit_name, unit.get_url()))
-			min_gear_level = player_unit and player_unit['gear'] or 1
-			for tier in reversed(range(min_gear_level, 13)):
-				sublines = []
-				for slot in sorted(data['tiers'][tier]):
-					gear_id = data['tiers'][tier][slot]['gear']
-					gear_url = data['tiers'][tier][slot]['url']
-					gear_name = translate(gear_id, language)
-					equipped = False
-					if player_unit:
-						for gear in player_unit['equipped']:
-							if tier == player_unit['gear']:
-								# gear['slot'] is an index, so add one for comparison
-								if int(slot) == gear['slot'] + 1:
-									equipped = True
-									break
+		jplayer = players[player.ally_code]
+		jroster = { x['defId']: x for x in jplayer['roster'] }
 
-					bold = not equipped and '**' or ''
+		for unit in selected_units:
 
-					sublines.append('%sSlot%s: [%s](%s)%s' % (bold, slot, gear_name, gear_url, bold))
+			lines = []
+			fields = []
+			player_unit = unit.base_id in jroster and jroster[unit.base_id] or {}
+			gear_levels = get_gear_levels(unit.base_id)
+			for name, data in gear_levels.items():
+				unit_name = translate(unit.base_id, language)
+				lines.append('**[%s](%s)**' % (unit_name, unit.get_url()))
+				min_gear_level = player_unit and player_unit['gear'] or 1
+				for tier in reversed(range(min_gear_level, 13)):
+					sublines = []
+					for slot in sorted(data['tiers'][tier]):
+						gear_id = data['tiers'][tier][slot]['gear']
+						gear_url = data['tiers'][tier][slot]['url']
+						gear_name = translate(gear_id, language)
+						equipped = False
+						if player_unit:
+							for gear in player_unit['equipped']:
+								if tier == player_unit['gear']:
+									# gear['slot'] is an index, so add one for comparison
+									print(json.dumps(gear, indent=4))
+									if int(slot) == gear['slot'] + 1:
+										equipped = True
+										break
 
-				gear_tier_id = 'Unit_Tier%02d' % tier
-				gear_tier_title = translate(gear_tier_id, language)
+						bold = not equipped and '**' or ''
 
-				fields.append({
-					'name': '== %s ==' % gear_tier_title,
-					'value': '\n'.join(sublines),
-				})
+						sublines.append('%sSlot%s: [%s](%s)%s' % (bold, slot, gear_name, gear_url, bold))
 
-		if not fields:
-			lines.append('Maximum gear level.')
+					gear_tier_id = 'Unit_Tier%02d' % tier
+					gear_tier_title = translate(gear_tier_id, language)
 
-		msgs.append({
-			#'title': '== Needed Gear ==',
-			'description': '\n'.join(lines),
-			'author': {
-				'name': unit.name,
-				'icon_url': unit.get_image(),
-			},
-			'image': {
-				'url': get_full_avatar_url(config, unit, player_unit),
-			},
-			'fields': fields,
-		})
+					fields.append({
+						'name': '== %s ==' % gear_tier_title,
+						'value': '\n'.join(sublines),
+					})
+
+			if not fields:
+				lines.append('Maximum gear level.')
+
+			msgs.append({
+				#'title': '== Needed Gear ==',
+				'description': '\n'.join(lines),
+				'author': {
+					'name': unit.name,
+					'icon_url': unit.get_image(),
+				},
+				'image': {
+					'url': get_full_avatar_url(config, unit, player_unit),
+				},
+				'fields': fields,
+			})
 
 	return msgs
