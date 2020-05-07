badb4961d4c01af1af2324a5805fe387c28c5c69
Corentin Delorme <codelorme@gmail.com>
2020-05-07 16:44:13 +0200

    Update examples/test-get-ability-name.py: Get rid of swgoh.help

--- examples/test-get-ability-name.py
+++ examples/test-get-ability-name.py
@@ -4,7 +4,7 @@ import os, sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 
 from config import load_config
-from swgohhelp import get_ability_name
+from utils import get_ability_name
 
 skill_id = 'leaderskill_VADER'
 language = 'eng_us'
