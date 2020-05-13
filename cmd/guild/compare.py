184981a83c20f4ff963c695ed3529bddf33bf4bd
Corentin Delorme <codelorme@gmail.com>
2020-05-13 21:32:11 +0200

    Update cmd/guild/compare.py: Fix bug with guild roster/members id/player_id keys

--- cmd/guild/compare.py
+++ cmd/guild/compare.py
@@ -182,8 +182,8 @@ async def cmd_guild_compare(request):
 		for guild_name, guild in result.items():
 
 			roster_for_unit = {}
-			guild_roster = { x['id']: x for x in guild['members'] }
-			for player_id, player in guild_roster.items():
+			members = { x['id']: x for x in guild['members'] }
+			for player_id, player in members.items():
 
 				player_roster = { x['defId']: x for x in player['roster'] }
 				for base_id, player_unit in player_roster.items():
