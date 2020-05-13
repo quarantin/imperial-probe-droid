af107ae8506313b3c28ef20d8a5051f2de49389c
Corentin Delorme <codelorme@gmail.com>
2020-05-13 04:35:07 +0200

    Update ticketscog.py: Refactoring + Add automatic ticket notifications

--- ticketscog.py
+++ ticketscog.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 
 import json
-from discord.ext import commands
+import traceback
+from datetime import datetime
+from discord.ext import commands, tasks
 
-import libswgoh
+from opts import *
+from errors import *
 
 import DJANGO
-from swgoh.models import Player
+from swgoh.models import Player, PremiumUser, PremiumUserConfig
 
 class TicketsCog(commands.Cog):
 
@@ -15,59 +18,41 @@ class TicketsCog(commands.Cog):
 		self.config = bot.config
 		self.logger = bot.logger
 		self.redis = bot.redis
+		self.notifier.start()
 
-	@commands.Cog.listener()
-	async def on_command_error(self, ctx, error):
+	def cog_unload(self):
+		self.notifier.cancel()
 
-		if isinstance(error, commands.CommandNotFound):
-			return
+	def get_notifications(self):
+		return PremiumUserConfig.objects.filter(key=PremiumUserConfig.CONFIG_NOTIFY_RAID_TICKETS)
 
-		raise error
+	def get_discord_id(self, player_id):
 
-	def get_discord_ids(self, player_id):
+		try:
+			player = Player.objects.get(player_id=player_id)
+			return '<@!%s>' % player.discord_id
 
-		key = 'player|%s' % player_id
-		player = self.redis.get(key)
-		if not player:
+		except Player.DoesNotExist:
 			return None
 
-		result = {}
-		player = json.loads(player.decode('utf-8'))
-		players = Player.objects.filter(ally_code=str(player['allyCode']))
-		for player in players:
-			result[player.game_nick] = player.discord_id
-
-		return result
-
-	@commands.group()
-	async def tickets(self, ctx, command=''):
+	async def get_tickets(self, creds_id=None, notify=False):
 
 		tickets = {
+			#'total': {},
 			'guild-tokens': {},
 			'raid-tickets': {},
 		}
 
 		total_guild_tokens = 0
 		total_raid_tickets = 0
-		session = await libswgoh.get_auth_google(creds_id='anraeth')
-		guild = await libswgoh.get_guild(session=session)
+		guild = await self.bot.client.guild(creds_id=creds_id, full=True)
 		for member in guild['roster']:
 
-			player_id = member['playerId']
-			profile_key = 'player|%s' % player_id
-			profile = self.redis.get(profile_key)
-			if not profile:
-				print('Could not find profile in redis: %s' % player_id)
-				continue
+			player_id = member['id']
+			discord_id = member['name']
 
-			profile = json.loads(profile.decode('utf-8'))
-
-			discord_id = profile['name']
-
-			if command.lower() in [ 'alert', 'mention', 'notify' ]:
-				discord_ids = self.get_discord_ids(player_id)
-				if discord_ids and discord_id in discord_ids:
-					discord_id = '<@!%s>' % discord_ids[discord_id]
+			if notify is True:
+				discord_id = self.get_discord_id(player_id) or member['name']
 
 			stat_gt = member['stats'][1] # Guild Token stats
 			stat_rt = member['stats'][2] # Raid Tickets stats
@@ -80,6 +65,61 @@ class TicketsCog(commands.Cog):
 
 			tickets['guild-tokens'][discord_id] = guild_tokens
 			tickets['raid-tickets'][discord_id] = raid_tickets
+
+		#tickets['total']['guild-tokens'] = total_guild_tokens
+		#tickets['total']['raid-tickets'] = total_raid_tickets
+
+		return tickets
+
+	@commands.Cog.listener()
+	async def on_command_error(self, ctx, error):
+
+		if isinstance(error, commands.CommandNotFound):
+			return
+
+		raise error
+
+	@tasks.loop(minutes=1)
+	async def notifier(self):
+
+		now = datetime.now()
+
+		notifications = self.get_notifications()
+		for notification in notifications:
+
+			notif_time = datetime.strptime(notification.value, '%H:%M')
+			premium_user = notification.premium_user
+
+			hour_ok = now.hour == notif_time.hour
+			minute_ok = now.minute == notif_time.minute
+			if hour_ok and minute_ok:
+
+				lines = []
+				guild_tickets = await self.get_tickets(creds_id=premium_user.creds_id, notify=True)
+				raid_tickets = guild_tickets['raid-tickets']
+
+				for name, tickets in sorted(raid_tickets.items(), key=lambda x: x[1], reverse=True):
+					if tickets != 600:
+						lines.append('`%d` __**%s**__' % (tickets, name))
+
+
+				channel = self.bot.get_channel(notification.channel_id)
+				await channel.send('\n'.join(lines))
+
+	@commands.group()
+	async def tickets(self, ctx, command=''):
+
+		tickets = {
+			'guild-tokens': {},
+			'raid-tickets': {},
+		}
+
+		premium_user = parse_opts_premium_user(ctx.author)
+		if not premium_user:
+			return error_not_premium()
+
+		notify = command.lower() in [ 'alert', 'mention', 'notify' ]
+		tickets = await self.get_tickets(creds_id=premium_user.creds_id, notify=notify)
 
 		lines = []
 		raid_tickets = tickets['raid-tickets']
