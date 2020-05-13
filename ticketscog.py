298fabdf46ad914636d9599f916387f9832e5cb0
Corentin Delorme <codelorme@gmail.com>
2020-05-13 18:49:18 +0200

    Update ticketscog.py: Add handling of Guild Tokens

--- ticketscog.py
+++ ticketscog.py
@@ -13,18 +13,51 @@ from swgoh.models import Player, PremiumUser, PremiumUserConfig
 
 class TicketsCog(commands.Cog):
 
+	max_raid_tickets = 600
+	max_guild_tokens = 10000
+
 	def __init__(self, bot):
 		self.bot = bot
 		self.config = bot.config
 		self.logger = bot.logger
 		self.redis = bot.redis
-		self.notifier.start()
+		self.raid_tickets_notifier.start()
 
 	def cog_unload(self):
-		self.notifier.cancel()
+		self.raid_tickets_notifier.cancel()
 
-	def get_notifications(self):
+	def get_lpad_tickets(self, tickets):
+
+		spaces = 0
+
+		if tickets < 10:
+			spaces = 2
+
+		elif tickets < 100:
+			spaces = 1
+
+		return ' ' * spaces
+
+	def get_lpad_tokens(self, tokens):
+
+		spaces = 0
+
+		if tokens < 10:
+			spaces = 3
+
+		elif tokens < 100:
+			spaces = 2
+
+		elif tokens < 1000:
+			spaces = 1
+
+		return ' ' * spaces
+
+	def get_raid_tickets_config(self):
 		return PremiumUserConfig.objects.filter(key=PremiumUserConfig.CONFIG_NOTIFY_RAID_TICKETS)
+
+	def get_guild_tokens_config(self):
+		return PremiumUserConfig.objects.filter(key=PremiumUserConfig.CONFIG_NOTIFY_GUILD_TOKENS)
 
 	def get_discord_id(self, player_id):
 
@@ -35,27 +68,27 @@ class TicketsCog(commands.Cog):
 		except Player.DoesNotExist:
 			return None
 
-	async def get_tickets(self, creds_id=None, notify=False):
+	async def get_guild_activity(self, creds_id=None, notify=False):
 
-		tickets = {
-			#'total': {},
+		guild_activity = {
+			'total': {},
 			'guild-tokens': {},
 			'raid-tickets': {},
 		}
 
 		total_guild_tokens = 0
 		total_raid_tickets = 0
-		guild = await self.bot.client.guild(creds_id=creds_id, full=True)
-		for member in guild['roster']:
+		guild = await self.bot.client.guild(creds_id=creds_id, full=True, debug=True)
+		for member, profile in zip(guild['roster'], guild['members']):
 
-			player_id = member['id']
-			discord_id = member['name']
+			player_id = profile['id']
+			discord_id = profile['name']
 
 			if notify is True:
-				discord_id = self.get_discord_id(player_id) or member['name']
+				discord_id = self.get_discord_id(player_id) or profile['name']
 
-			stat_gt = member['stats'][1] # Guild Token stats
-			stat_rt = member['stats'][2] # Raid Tickets stats
+			stat_gt = member['activities'][1] # Guild Tokens stats
+			stat_rt = member['activities'][2] # Raid Tickets stats
 
 			guild_tokens = 'valueDaily' in stat_gt and stat_gt['valueDaily'] or 0
 			raid_tickets = 'valueDaily' in stat_rt and stat_rt['valueDaily'] or 0
@@ -63,13 +96,13 @@ class TicketsCog(commands.Cog):
 			total_guild_tokens += guild_tokens
 			total_raid_tickets += raid_tickets
 
-			tickets['guild-tokens'][discord_id] = guild_tokens
-			tickets['raid-tickets'][discord_id] = raid_tickets
+			guild_activity['guild-tokens'][discord_id] = guild_tokens
+			guild_activity['raid-tickets'][discord_id] = raid_tickets
 
-		#tickets['total']['guild-tokens'] = total_guild_tokens
-		#tickets['total']['raid-tickets'] = total_raid_tickets
+		guild_activity['total']['guild-tokens'] = total_guild_tokens
+		guild_activity['total']['raid-tickets'] = total_raid_tickets
 
-		return tickets
+		return guild_activity
 
 	@commands.Cog.listener()
 	async def on_command_error(self, ctx, error):
@@ -80,55 +113,110 @@ class TicketsCog(commands.Cog):
 		raise error
 
 	@tasks.loop(minutes=1)
-	async def notifier(self):
+	async def raid_tickets_notifier(self, min_tickets: int = 600):
 
 		now = datetime.now()
 
-		notifications = self.get_notifications()
-		for notification in notifications:
+		alerts = self.get_raid_tickets_config()
+		for alert in alerts:
 
-			notif_time = datetime.strptime(notification.value, '%H:%M')
-			premium_user = notification.premium_user
+			notif_time = datetime.strptime(alert.value, '%H:%M')
+			premium_user = alert.premium_user
 
 			hour_ok = now.hour == notif_time.hour
 			minute_ok = now.minute == notif_time.minute
 			if hour_ok and minute_ok:
 
 				lines = []
-				guild_tickets = await self.get_tickets(creds_id=premium_user.creds_id, notify=True)
-				raid_tickets = guild_tickets['raid-tickets']
+				guild_activity = await self.get_guild_activity(creds_id=premium_user.creds_id, notify=True)
+				raid_tickets = guild_activity['raid-tickets']
 
 				for name, tickets in sorted(raid_tickets.items(), key=lambda x: x[1], reverse=True):
-					if tickets != 600:
-						lines.append('`%d` __**%s**__' % (tickets, name))
+					if tickets < min_tickets:
+						pad = self.get_lpad_tickets(tickets)
+						lines.append('`| %s%d/%d |` __**%s**__' % (pad, tickets, min_tickets, name))
 
-
-				channel = self.bot.get_channel(notification.channel_id)
+				channel = self.bot.get_channel(alert.channel_id)
 				await channel.send('\n'.join(lines))
 
-	@commands.group()
-	async def tickets(self, ctx, command=''):
+	@tasks.loop(minutes=1)
+	async def guild_tokens_notifier(self, min_tokens: int = 10000):
 
-		tickets = {
-			'guild-tokens': {},
-			'raid-tickets': {},
-		}
+		now = datetime.now()
+
+		alerts = self.get_guild_tokens_config()
+		for alert in alerts:
+
+			notif_time = datetime.strptime(alert.value, '%H:%M')
+			premium_user = alert.premium_user
+
+			hour_ok = now.hour == notif_time.hour
+			minute_ok = now.minute == notif_time.minute
+			if hour_ok and minute_ok:
+
+				lines = []
+				guild_activity = await self.get_guild_activity(creds_id=premium_user.creds_id, notify=True)
+				guild_tokens = guild_activity['guild-tokens']
+
+				for name, tokens in sorted(guild_tokens.items(), key=lambda x: x[1], reverse=True):
+					if tickets != self.max_raid_tickets:
+						lines.append('`| %s%d/%d |` __**%s**__' % (pad, tickets, min_tickets, name))
+
+				channel = self.bot.get_channel(alert.channel_id)
+				await channel.send('\n'.join(lines))
+
+	@commands.command(aliases=['rtc'])
+	async def raid_tickets_check(self, ctx, min_tickets: int = 600, *, command: str = ''):
+
+		if min_tickets > self.max_raid_tickets:
+			min_tickets = self.max_raid_tickets
 
 		premium_user = parse_opts_premium_user(ctx.author)
 		if not premium_user:
 			return error_not_premium()
 
-		notify = command.lower() in [ 'alert', 'mention', 'notify' ]
-		tickets = await self.get_tickets(creds_id=premium_user.creds_id, notify=notify)
+		notify = command.lower() in [ 'alert', 'alerts', 'mention', 'mentions', 'notify', 'notification', 'notifications' ]
 
 		lines = []
-		raid_tickets = tickets['raid-tickets']
+		guild_activity = await self.get_guild_activity(creds_id=premium_user.creds_id, notify=notify)
+		raid_tickets = guild_activity['raid-tickets']
 		for name, tickets in sorted(raid_tickets.items(), key=lambda x: x[1], reverse=True):
-			if tickets != 600:
-				lines.append('`%d` __**%s**__' % (tickets, name))
+			if tickets < min_tickets :
+				pad = self.get_lpad_tickets(tickets)
+				lines.append('`| %s%d/%d |` __**%s**__' % (pad, tickets, min_tickets, name))
+
+		if not lines:
+			lines.append('Everyone got their raid tickets done (%d/%d)! Congratulations! 🥳' % (min_tickets, self.max_raid_tickets))
+		else:
+			sep = self.config['separator']
+			lines = [ sep ] + lines + [ sep ]
 
 		await ctx.send('\n'.join(lines))
 
-	@tickets.command()
-	async def addguild(self, ctx):
-		pass
+	@commands.command(aliases=['gtc'])
+	async def guild_tokens_check(self, ctx, min_tokens: int = 10000, *, command=''):
+
+		if min_tokens > self.max_guild_tokens:
+			min_tokens = self.max_guild_tokens
+
+		premium_user = parse_opts_premium_user(ctx.author)
+		if not premium_user:
+			return error_not_premium()
+
+		notify = command.lower() in [ 'alert', 'mention', 'notify', 'notification' ]
+
+		lines = []
+		guild_activity = await self.get_guild_activity(creds_id=premium_user.creds_id, notify=notify)
+		guild_tokens = guild_activity['guild-tokens']
+		for name, tokens in sorted(guild_tokens.items(), key=lambda x: x[1], reverse=True):
+			if tokens < min_tokens:
+				pad = self.get_lpad_tokens(tokens)
+				lines.append('`| %s%d/%d |` __**%s**__' % (pad, tokens, min_tokens, name))
+
+		if not lines:
+			lines.append('Everyone got their guild tokens done (%d/%d)! Contratulations! 🥳' % (min_tokens, self.max_guild_tokens))
+		else:
+			sep = self.config['separator']
+			lines = [ sep ] + lines + [ sep ]
+
+		await ctx.send('\n'.join(lines))
