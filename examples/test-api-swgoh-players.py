838c3507f9e79e152aafe183241c68707da69202
Corentin Delorme <codelorme@gmail.com>
2020-05-17 17:45:41 +0200

    Update examples/test-api-swgoh-players.py: Use new version of swgohhelp module (now it's a class)

--- examples/test-api-swgoh-players.py
+++ examples/test-api-swgoh-players.py
@@ -5,12 +5,14 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 
 import json
 from config import load_config
-from swgohhelp import api_swgoh_players
+from swgohhelp import SwgohHelp
 
 async def __main__():
 	config = load_config()
+	client = SwgohHelp(config)
+
 	ally_codes = [ 349423868 ]
-	result = await api_swgoh_players(config, { 'allycodes': ally_codes })
+	result = await client.api_swgoh_players({ 'allycodes': ally_codes })
 	print(json.dumps(result, indent=4))
 
 if __name__ == '__main__':
