0f58c71508e6db6a66d236fe8d69a0ac429fd22e
Corentin Delorme <codelorme@gmail.com>
2020-06-19 21:50:47 +0200

    Update tbcog.py: Add command tbhist

--- tbcog.py
+++ tbcog.py
@@ -1,10 +1,9 @@
-#!/usr/bin/env python3
-
 import io
 import os
 import json
 import asyncio
 import zipfile
+import traceback
 from datetime import datetime
 
 import discord
@@ -15,8 +14,9 @@ import libswgoh
 from utils import translate, translate_multi
 
 import DJANGO
-from swgoh.models import Player
-from territorybattle.models import TerritoryBattleHistory
+from django.db import transaction
+from swgoh.models import Guild, Player
+from territorybattle.models import TerritoryBattle, TerritoryBattleHistory
 
 class TBCog(cog.Cog):
 
@@ -29,6 +29,47 @@ class TBCog(cog.Cog):
 		raise error
 
 	@commands.command()
+	async def tbhist(self, ctx):
+
+		player = self.options.parse_premium_user(ctx)
+		if not player:
+			return self.errors.not_premium()
+
+		last_event = TerritoryBattle.objects.first()
+		data = await self.client.get_tb_history(creds_id=player.creds_id, last_event_id=last_event.event_id)
+		if not data:
+			return self.bot.errors.tb_history_failed()
+
+		guild_id = data['guild_id']
+		guild_name = data['guild_name']
+		history = data['history']
+		event_id = data['event_id']
+
+		print('Parsing history for TW %s' % event_id)
+
+		errors = False
+		with transaction.atomic():
+
+			guild, created = Guild.objects.get_or_create(guild_id=guild_id, guild_name=guild_name)
+			for event in history:
+
+				if event['type'] not in [ 'TERRITORY_CONFLICT_ACTIVITY' ]:
+					continue
+
+				try:
+					o, created = TerritoryBattleHistory.parse(guild, event)
+					print('Parsing %s event %s' % (event['id'], created and 'new' or 'old'))
+
+				except Exception as err:
+					errors = True
+					print('Some error detected!')
+					print(err)
+					print(traceback.format_exc())
+					print(json.dumps(event, indent=4))
+
+
+		await ctx.send(errors and 'KO' or 'OK')
+
 	async def history(self, ctx, creds_id='anraeth'):
 		
 		event_id = 'TB_EVENT_GEONOSIS_SEPARATIST:O1588352400000'
@@ -60,11 +101,3 @@ class TBCog(cog.Cog):
 
 		print(json.dumps(defend_orders, indent=4))
 		print(json.dumps(attack_orders, indent=4))
-
-async def main():
-	#cog = TBCog(None)
-	#session = await libswgoh.get_auth_google(creds_id='anraeth')
-	pass
-
-if __name__ == '__main__':
-	asyncio.get_event_loop().run_until_complete(main())
