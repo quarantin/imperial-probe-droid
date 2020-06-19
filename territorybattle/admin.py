364d787f0657a069a39f365a60553424d8992b80
Corentin Delorme <codelorme@gmail.com>
2020-06-19 13:01:13 +0200

    Update territorybattle/admin.py: Fix field name: 'event_type' => 'activity'

--- territorybattle/admin.py
+++ territorybattle/admin.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 from .models import TerritoryBattleHistory
 
 class TerritoryBattleHistoryAdmin(admin.ModelAdmin):
-	list_display = [ 'id', 'event_type', 'player_name', 'phase', 'territory' ]
+	list_display = [ 'id', 'activity', 'player_name', 'phase', 'territory' ]
 
 admin.site.register(TerritoryBattleHistory, TerritoryBattleHistoryAdmin)
